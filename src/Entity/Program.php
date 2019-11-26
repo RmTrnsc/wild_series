1fa658e303a9da84278cf1e5794130bc1d4a12f7
romain <tournesacromain@gmail.com>
2019-11-26 19:26:31 +0100

    Quest symfony 7

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -9,66 +9,76 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Program
 {
-    /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+  /**
+   * @ORM\Id()
+   * @ORM\GeneratedValue()
+   * @ORM\Column(type="integer")
+   */
+  private $id;
+  /**
+   * @ORM\Column(type="string", length=255)
+   */
+  private $title;
+  /**
+   * @ORM\Column(type="text")
+   */
+  private $summary;
+  /**
+   * @ORM\Column(type="string", length=255, nullable=true)
+   */
+  private $poster;
+  /**
+   * @ORM\ManyToOne(targetEntity="App\Entity\Category")
+   * @ORM\JoinColumn(nullable=false)
+   */
+  private $category;
 
-    /**
-     * @ORM\Column(type="string", length=190)
-     */
-    private $title;
+  public function getId(): ?int
+  {
+    return $this->id;
+  }
 
-    /**
-     * @ORM\Column(type="text")
-     */
-    private $summary;
+  public function getTitle(): ?string
+  {
+    return $this->title;
+  }
 
-    /**
-     * @ORM\Column(type="string", length=190, nullable=true)
-     */
-    private $poster;
+  public function setTitle(string $title): self
+  {
+    $this->title = $title;
+    return $this;
+  }
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+  public function getSummary(): ?string
+  {
+    return $this->summary;
+  }
 
-    public function getTitle(): ?string
-    {
-        return $this->title;
-    }
+  public function setSummary(string $summary): self
+  {
+    $this->summary = $summary;
+    return $this;
+  }
 
-    public function setTitle(string $title): self
-    {
-        $this->title = $title;
+  public function getPoster(): ?string
+  {
+    return $this->poster;
+  }
 
-        return $this;
-    }
+  public function setPoster(?string $poster): self
+  {
+    $this->poster = $poster;
+    return $this;
+  }
 
-    public function getSummary(): ?string
-    {
-        return $this->summary;
-    }
+  public function getCategory(): ?Category
+  {
+    return $this->category;
+  }
 
-    public function setSummary(string $summary): self
-    {
-        $this->summary = $summary;
-
-        return $this;
-    }
-
-    public function getPoster(): ?string
-    {
-        return $this->poster;
-    }
-
-    public function setPoster(?string $poster): self
-    {
-        $this->poster = $poster;
-
-        return $this;
-    }
+  public function setCategory(?Category $category): self
+  {
+    $this->category = $category;
+    return $this;
+  }
 }
--- src/Migrations/Version20191126152754.php
+++ src/Migrations/Version20191126152754.php
@@ -23,7 +23,7 @@ final class Version20191126152754 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE program (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(190) NOT NULL, summary LONGTEXT NOT NULL, poster VARCHAR(190) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE program (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(190) NOT NULL, summary LONGTEXT NOT NULL, poster VARCHAR(190) DEFAULT NULL,  PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
