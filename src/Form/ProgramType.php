6350b08bcaa99e4f3de5e4424539debe11a4fe67
romain <tournesacromain@gmail.com>
2020-02-13 16:26:00 +0100

    Minor change

--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Actor;
+use App\Entity\Category;
 use App\Entity\Program;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
@@ -18,30 +19,31 @@ class ProgramType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title', TextType::class)
-            ->add('summary', TextareaType::class)
-            ->add('poster', UrlType::class)
-            ->add('category', null, [
-                'choice_label' => 'name'
-            ])
-            ->add('actors', EntityType::class, [
-                    'class' => Actor::class,
-                    'query_builder' => function (EntityRepository $er) {
-                        return $er->createQueryBuilder('u')
-                            ->orderBy('u.name', 'asc');
-                    },
-                    'multiple' => true,
-                    'expanded' => true,
-                    'choice_label' => 'name',
-                    'by_reference' =>false
-                ]
-            );
+          ->add('title', TextType::class)
+          ->add('summary', TextareaType::class)
+          ->add('poster', UrlType::class)
+          ->add('category', EntityType::class, [
+            'class' => Category::class,
+            'choice_label' => 'name'
+          ])
+          ->add('actors', EntityType::class, [
+              'class' => Actor::class,
+              'query_builder' => function (EntityRepository $er) {
+                  return $er->createQueryBuilder('u')
+                    ->orderBy('u.name', 'asc');
+              },
+              'multiple' => true,
+              'expanded' => true,
+              'choice_label' => 'name',
+              'by_reference' => false
+            ]
+          );
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Program::class,
+          'data_class' => Program::class,
         ]);
     }
 }
